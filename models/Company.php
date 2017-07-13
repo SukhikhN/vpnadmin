9c47f99475bf960e2745de9bd2af73f0ae26fa96
Sukhikh Nickolay <_mk_@ukr.net>
2017-07-14 00:29:03 +0300

    Add generated models for Company and User

---
+++ models/Company.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "company".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $quota
+ *
+ * @property User[] $users
+ */
+class Company extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'company';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'quota'], 'required'],
+            [['quota'], 'integer'],
+            [['name'], 'string', 'max' => 500],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'quota' => 'Traffic quota',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsers()
+    {
+        return $this->hasMany(User::className(), ['company_id' => 'id']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return CompanyQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new CompanyQuery(get_called_class());
+    }
+}
---
+++ models/User.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $email
+ * @property string $name
+ * @property integer $company_id
+ *
+ * @property Company $company
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['email', 'name', 'company_id'], 'required'],
+            [['company_id'], 'integer'],
+            [['email'], 'string', 'max' => 255],
+            [['name'], 'string', 'max' => 500],
+            [['company_id'], 'exist', 'skipOnError' => true, 'targetClass' => Company::className(), 'targetAttribute' => ['company_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'EMail',
+            'name' => 'Name',
+            'company_id' => 'Company ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCompany()
+    {
+        return $this->hasOne(Company::className(), ['id' => 'company_id']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return UserQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new UserQuery(get_called_class());
+    }
+}
